dfa79733e15edba48ebcf48ccdf1b87e1c80b849
Luiz Chun <53568746+luizchun@users.noreply.github.com>
2022-09-01 00:37:00 -0300

    Create Draw The Triangle 1.sql

---
+++ Draw The Triangle 1.sql
@@ -0,0 +1,19 @@
+/*
+P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+Write a query to print the pattern P(20).
+*/
+
+
+DECLARE @cnt INT = 20;
+DECLARE @STR AS VARCHAR(50) = '* * * * * * * * * * * * * * * * * * * *';
+
+WHILE @cnt != 0
+BEGIN
+   SELECT SUBSTRING(@STR, 1, @cnt*2) AS ExtractString;
+   SET @cnt = @cnt - 1;
+END;
